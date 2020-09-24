252b556ecd70ea394fdf1b482a0b6773ceb462c5
sindhubedare <sindhubedare1990@gmail.com>
2020-09-24 17:55:33 -0500

    navbar responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-// import Navbar from './Components/Navbar';
 import Newnav from './Components/Newnav';
 
 
@@ -8,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Navbar /> */}
+       
       <Newnav />
     
       </header>
